cf2be962c2378343db54b691ddb4ddc1f70d2f61
Li Mu <61442081+yixiay@users.noreply.github.com>
2023-04-21 18:40:05 +0800

    ipinfo.js
    ipinfo

---
+++ ipinfo.js
@@ -0,0 +1,46 @@
+var title = '        馃攷 IP Info'
+
+$task.fetch({
+    url: 'http://ip.343.re/info',
+    timeout: 4000,
+    opts: {
+        policy: $environment.params
+    }
+}).then(response => {
+    $done({
+        'title': title,
+        'htmlMessage': response ? format(JSON.parse(response.body)) : ''
+    });
+}, reason => {
+    $done({
+        'title': title,
+        'htmlMessage': errMsg('节点超时？？')
+    });
+})
+
+function errMsg(reason) {
+    return `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: bold;"></br></br>` + reason + `</p>`
+}
+
+function format(info) {
+    if (info['status'] !== 'T') {
+        return errMsg('馃洃 鏌ヨ澶辫触')
+    }
+    field = {
+        'ip': 'IP',
+        'as': 'AS',
+        'city': 'City',
+        'region': 'Region',
+        'country': 'Country',
+        'timezone': 'Timezone',
+        'loc': 'Location',
+        'isp': 'ISP',
+        'scope': 'Scope',
+        'detail': 'Detail'
+    }
+    var html = ''
+    for (index in field) {
+        html += '</br></br><b><font>' + field[index] + '</font> : </b><font>' + info[index] + '</font>'
+    }
+    return `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">` + html + `</p>`
+}
